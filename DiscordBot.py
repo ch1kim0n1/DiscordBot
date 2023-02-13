02198a6f63801d8161752aecaac92b5edead3916
ch1kim0n1 <chikimoni61@gmail.com>
2023-02-13 17:20:00 -0600

    update

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,14 +1,27 @@
 import discord
 import random
 
-intents = discord.Intents.default()
-intents.messages = True
-intents.reactions = True
+client = discord.Client()
 
-client = discord.Client(intents=intents)
+# List of music recommendations
+music_list = ['Rock', 'Pop', 'Jazz', 'Classical', 'Hip Hop', 'Country']
 
-anime_library = ['Action', 'Comedy', 'Drama', 'Romance', 'Fantasy', 'Mystery', 'Horror', 'Thriller', 'Adventure']
-music_library = ['Rock', 'Pop', 'Jazz', 'Classical', 'Rap', 'Hip Hop', 'Blues', 'Metal', 'Country']
+# List of anime genres
+anime_genres = ['Action', 'Comedy', 'Romance', 'Fantasy', 'Drama', 'Mystery/Thriller', 'Historical', 'Sci-Fi', 'Horror', 'Real-Life']
+
+# Dictionary of anime survey questions and corresponding anime genres
+anime_questions = {
+    'Do you prefer action-packed anime?': 'Action',
+    'Do you enjoy comedies in anime?': 'Comedy',
+    'Would you rather watch a romance anime?': 'Romance',
+    'Do you like anime that has a lot of fantasy elements?': 'Fantasy',
+    'Do you prefer a more serious, drama-filled anime?': 'Drama',
+    'Would you enjoy a mystery or thriller anime?': 'Mystery/Thriller',
+    'Do you like anime that is set in the past?': 'Historical',
+    'Do you like anime that is set in the future?': 'Sci-Fi',
+    'Would you watch a horror anime?': 'Horror',
+    'Do you like anime that is based on real-life events?': 'Real-Life'
+}
 
 @client.event
 async def on_ready():
@@ -20,22 +33,24 @@ async def on_message(message):
         return
 
     if message.content.startswith('!music'):
-        music = random.choice(music_library)
-        await message.channel.send(f'How about some {music} music?')
+        music_recommendation = random.choice(music_list)
+        await message.channel.send(f'Here is a music recommendation: {music_recommendation}')
+
+    elif message.content.startswith('!manga'):
+        manga_page = random.randint(1, 150)
+        await message.channel.send(f'Why not read page {manga_page} of your manga today?')
 
     elif message.content.startswith('!anime'):
-        anime = random.choice(anime_library)
-        await message.channel.send(f'You should check out some {anime} anime!')
+        await message.channel.send('Let\'s determine what genre of anime you should watch. Answer the following questions with either "yes" or "no".')
+        
+        user_preferences = {}
+        for question in anime_questions:
+            response = await client.wait_for('message', check=lambda m: m.author == message.author and (m.content.lower() == 'yes' or m.content.lower() == 'no'))
+            user_preferences[anime_questions[question]] = (1 if response.content.lower() == 'yes' else 0)
+        
+        recommended_genre = max(user_preferences, key=user_preferences.get)
+        await message.channel.send(f'Based on your answers, it looks like you might enjoy watching anime in the {recommended_genre} genre.')
 
-    elif message.content.startswith('!randomM'):
-        page = random.randint(1, 100)
-        await message.channel.send(f'How about opening a random manga page? How about page {page}?')
+client.run('YOUR_BOT_TOKEN_HERE')
 
-    elif message.content.startswith('!help'):
-        await message.channel.send('Commands:\n!music - recommends a random music genre\n!anime - recommends a random anime genre\n!randomM - suggests opening a random manga page\n!help - shows this help message')
-
-    else:
-        await message.channel.send('Error: Invalid command. Type !help for a list of available commands.')
-
-client.run('TOKEN')
 
