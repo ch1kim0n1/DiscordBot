68d653d35b9b835461ab1e9b33f471733e5956b4
ch1kim0n1 <chikimoni61@gmail.com>
2023-02-21 14:55:41 -0600

    quiz game

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,26 +1,37 @@
 import discord
+import requests
 import random
 
 client = discord.Client()
 
-# List of music recommendations
-music_list = ['Rock', 'Pop', 'Jazz', 'Classical', 'Hip Hop', 'Country']
-
-# List of anime genres
-anime_genres = ['Action', 'Comedy', 'Romance', 'Fantasy', 'Drama', 'Mystery/Thriller', 'Historical', 'Sci-Fi', 'Horror', 'Real-Life']
-
-# Dictionary of anime survey questions and corresponding anime genres
-anime_questions = {
-    'Do you prefer action-packed anime?': 'Action',
-    'Do you enjoy comedies in anime?': 'Comedy',
-    'Would you rather watch a romance anime?': 'Romance',
-    'Do you like anime that has a lot of fantasy elements?': 'Fantasy',
-    'Do you prefer a more serious, drama-filled anime?': 'Drama',
-    'Would you enjoy a mystery or thriller anime?': 'Mystery/Thriller',
-    'Do you like anime that is set in the past?': 'Historical',
-    'Do you like anime that is set in the future?': 'Sci-Fi',
-    'Would you watch a horror anime?': 'Horror',
-    'Do you like anime that is based on real-life events?': 'Real-Life'
+# List of 25 questions to determine if someone is a yandere or tsundere lover
+questions = {
+    'Do you prefer characters who are outgoing or reserved?': 'tsundere',
+    'Do you like it when characters act possessive of their love interest?': 'yandere',
+    'Do you enjoy characters who have a short temper?': 'tsundere',
+    'Do you like it when characters are overly affectionate with their love interest?': 'yandere',
+    'Do you enjoy characters who are sarcastic?': 'tsundere',
+    'Do you like it when characters are overly jealous?': 'yandere',
+    'Do you prefer characters who are independent or dependent?': 'tsundere',
+    'Do you like it when characters are obsessive about their love interest?': 'yandere',
+    'Do you enjoy characters who are cold and distant?': 'tsundere',
+    'Do you like it when characters are possessive of their friends?': 'yandere',
+    'Do you prefer characters who are straightforward or indirect?': 'tsundere',
+    'Do you like it when characters are willing to do anything for their love interest?': 'yandere',
+    'Do you enjoy characters who are teasing or playful?': 'tsundere',
+    'Do you like it when characters are violent towards others who threaten their love interest?': 'yandere',
+    'Do you prefer characters who are calm or emotional?': 'tsundere',
+    'Do you like it when characters are willing to hurt others to protect their love interest?': 'yandere',
+    'Do you enjoy characters who are flustered or embarrassed?': 'tsundere',
+    'Do you like it when characters are willing to kill others who threaten their love interest?': 'yandere',
+    'Do you prefer characters who are logical or impulsive?': 'tsundere',
+    'Do you like it when characters are manipulative to protect their love interest?': 'yandere',
+    'Do you enjoy characters who are blunt or tactful?': 'tsundere',
+    'Do you like it when characters are willing to manipulate others to protect their love interest?': 'yandere',
+    'Do you prefer characters who are serious or playful?': 'tsundere',
+    'Do you like it when characters are willing to die for their love interest?': 'yandere',
+    'Do you enjoy characters who are honest or deceitful?': 'tsundere',
+    'Do you like it when characters are willing to go to extreme lengths to protect their love interest?': 'yandere'
 }
 
 @client.event
@@ -28,29 +39,31 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
 
 @client.event
+async def on_ready():
+    print('Bot is ready.')
+
+@client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!music'):
-        music_recommendation = random.choice(music_list)
-        await message.channel.send(f'Here is a music recommendation: {music_recommendation}')
-
-    elif message.content.startswith('!manga'):
-        manga_page = random.randint(1, 150)
-        await message.channel.send(f'Why not read page {manga_page} of your manga today?')
-
-    elif message.content.startswith('!anime'):
-        await message.channel.send('Let\'s determine what genre of anime you should watch. Answer the following questions with either "yes" or "no".')
+    if message.content.startswith('!animegif'):
+        api_key = 'YOUR_API_KEY'
+        query = 'anime'
+        url = f'https://api.giphy.com/v1/gifs/search?api_key={api_key}&q={query}&limit=50&offset=0&rating=g&lang=en'
+        response = requests.get(url)
+        data = response.json()
+        gif = random.choice(data['data'])['images']['original']['url']
+        await message.channel.send(gif)
+    
+    if message.content.startswith('!quiz'):
+        scores = {'tsundere': 0, 'yandere': 0}
         
-        user_preferences = {}
-        for question in anime_questions:
-            response = await client.wait_for('message', check=lambda m: m.author == message.author and (m.content.lower() == 'yes' or m.content.lower() == 'no'))
-            user_preferences[anime_questions[question]] = (1 if response.content.lower() == 'yes' else 0)
+        for i in range(25):
+            scores[random.choice(['tsundere', 'yandere'])] += 1
         
-        recommended_genre = max(user_preferences, key=user_preferences.get)
-        await message.channel.send(f'Based on your answers, it looks like you might enjoy watching anime in the {recommended_genre} genre.')
+        tsundere_percentage = int(scores['tsundere'] / 25 * 100)
+        yandere_percentage = int(scores['yandere'] / 25 * 100)
 
-client.run('YOUR_BOT_TOKEN_HERE')
+        await message.channel.send(f'Hello there, after I looked at your responses, I am shocked.\n\n- {yandere_percentage}% Yandere Lover\n- {tsundere_percentage}% Tsundere Lover\n\n*Wow, you\'re quite the interesting one, aren\'t you?*')
+        
+client.run('YOUR_DISCORD_BOT_TOKEN')
 
 
